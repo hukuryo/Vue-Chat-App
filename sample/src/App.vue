a685e1633aef17a09d3d146581daacd7a3940923
福島 涼平 <xfukushima@sekisan.i-juse.co.jp>
2023-04-11 16:07:28 +0900

    レイアウトを整えました

--- sample/src/App.vue
+++ sample/src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<SetHeader />
+  <TopIndex />
+  <SetFooter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SetHeader from '@/components/Common/SetHeader.vue'
+import SetFooter from '@/components/Common/SetFooter.vue'
+import TopIndex from '@/components/TopIndex.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SetFooter,
+    TopIndex,
+    SetHeader
   }
 }
 </script>
@@ -21,6 +29,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ sample/src/components/TopIndex.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>ボディ</h1>
+    
+</template>
+<style>
+.footerWrap { 
+font-size: 20px;
+}
+</style>
